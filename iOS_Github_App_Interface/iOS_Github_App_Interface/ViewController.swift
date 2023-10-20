1ead9a9c56a14054dcd6beafe961a47a82f73019
erkutahmet <90329328+erkutahmet@users.noreply.github.com>
2023-10-21 00:02:24 +0300

    created iOS Github App Interface xcode project

---
+++ iOS_Github_App_Interface/iOS_Github_App_Interface/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iOS_Github_App_Interface
+//
+//  Created by Ahmet Erkut on 21.10.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
